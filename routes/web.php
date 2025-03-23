3936d99ce43fe179bdb3f3b9233583f8254ce3b7
DIDAN <blueocean.didan@gmail.com>
2025-03-23 21:33:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\meowcontroller;
 use App\Http\Controllers\ListbarangController;
-use App\Http\Controllers\WhiteController;
+use App\Http\Controllers\whiteController;
 
 Route::get('/welcome', function () {
     return view('welcome');
