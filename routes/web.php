30c6430b4ca8f3ff9a2a787c780138e4d477becf
kimdaedan <blueocean.didan@gmail.com>
2025-03-23 19:42:31 +0700

    update terbaru

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\meowcontroller;
+use App\Http\Controllers\ListbarangController;
 
-// Route::get('/', function () {
-//    return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+ });
+
+ Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID' . $id;
+ });
+
+ Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin User';
+    });
+});
 
 Route::get('/', [meowcontroller::class, 'index']);
 Route::get('/contact', [meowcontroller::class, 'contact']);
+
+//Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+//    return view('list_barang', compact('id', 'nama'));
+// });
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
